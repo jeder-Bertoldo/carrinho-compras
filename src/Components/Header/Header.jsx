25a318dbbbea8ad402c30bf203d499d3624eb342
jeder-Bertoldo <115741406+jeder-Bertoldo@users.noreply.github.com>
2024-01-18 19:11:56 -0500

    Componete Header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+
+import './Header.css';
+
+function Header(){
+  return (
+
+    <header className="header">
+      <div className="container">
+        <SearchBar/>
+        <button>cart</button>
+      </div>
+
+
+    </header>
+
+  );
+ 
+}
+export default Header;
