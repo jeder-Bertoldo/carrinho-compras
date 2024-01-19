9ce4b5a617f1c36132fbd421f88b0e05e420442f
jeder-Bertoldo <115741406+jeder-Bertoldo@users.noreply.github.com>
2024-01-19 15:59:18 -0500

    nova barra de pesquisa

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { IoIosSearch } from 'react-icons/io';
 
+import './SearchBar.css';
+
 function SearchBar(){
-  return(
-    <form>
-      <input type="search" placeholder="Buscar produtos"
+
+  const [searchValue ,setSearchValue] = useState('');
+
+  return (
+    <form className="search-bar">
+      <input type="search"
+        value={searchValue}
+        placeholder="Buscar produtos"
         className="search_input" 
+        onChange= {({target}) => setSearchValue(target.value)}
         required
       />
+  
       <button type="submit" className="search_button">
         <IoIosSearch/>
       </button>
