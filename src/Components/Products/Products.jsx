b4cb0fe4d1aa1615bb76958e162dbb80f6e0fc2b
jeder-Bertoldo <115741406+jeder-Bertoldo@users.noreply.github.com>
2024-01-20 19:09:58 -0500

    stile produto

---
+++ src/Components/Products/Products.jsx
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from 'react';
+
+import './Products.css';
+import fetchProducts from '../../api/fetchProducts';
+function Products() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=> {
+    
+    fetchProducts('iphone').then((Response)=> {
+      setProducts(Response);
+    });
+
+  }, []);
+
+  return (
+    <section className="products container">
+      {
+        products.map((product) => <p key={product.title}>{product.title}</p>)
+      }
+    </section>
+  );
+}
+
+export default Products;
