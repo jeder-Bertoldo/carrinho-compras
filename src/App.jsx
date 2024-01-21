1b560c3f10b2c712c56c31628a88419e884ca9cb
jeder-Bertoldo <115741406+jeder-Bertoldo@users.noreply.github.com>
2024-01-20 19:05:31 -0500

    novo componete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Components/Header/Header';
+import Products from './Components/Products/Products';
 
 function App() {
   return (
     <div>
       <Header />
+      <Products />
     </div>
   );
 }
