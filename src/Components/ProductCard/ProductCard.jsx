493e92f75359417b9a506914320addff1f041076
jeder-Bertoldo <115741406+jeder-Bertoldo@users.noreply.github.com>
2024-01-22 15:08:42 -0500

    componente currancy

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import { FaCartPlus } from 'react-icons/fa';
 import './ProductCard.css';
+import formatCurrancy from '../../utils/formatCurrency';
 
-function ProductCard(){
+
+function ProductCard({data}){
+
+  const  { title, thumbnail, price} = data;
+
   return (
     <section className="product-card">
    
-      <img src="https://http2.mlstatic.com/D_918579-MLM51559384401_092022-I.jpg"
+      <img src={thumbnail}
         alt="Product" 
         className="card_image"/>
 
       <div className="card_infos">
-        <h2 className="card_price">R$ 200,20</h2>
-        <h2 className="card_title">Manual do mundo</h2>
+        <h2 className="card_price">{formatCurrancy(price, 'BRL')}</h2>
+        <h2 className="card_title">{title}</h2>
       </div>
 
       <button type="button" className="button_add-cart">
@@ -24,3 +30,7 @@ function ProductCard(){
 }
 
 export default ProductCard;
+
+ProductCard.propTypes = {
+  data: propTypes.shape({}),
+}.isRequired;
