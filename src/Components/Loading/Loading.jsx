93fd419768671bdbbfa3c66255d225f2876b0b75
jeder-Bertoldo <115741406+jeder-Bertoldo@users.noreply.github.com>
2024-01-24 15:58:51 -0500

    Efeito de login

---
+++ src/Components/Loading/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { RiLoader2Fill } from 'react-icons/ri';
+import './Loading.css';
+
+function Loading(){
+  return ( <RiLoader2Fill className="loading"/>
+  );
+}
+
+export default Loading;
