540f108002095df80f683568b306980f5a9f3977
jeder-Bertoldo <115741406+jeder-Bertoldo@users.noreply.github.com>
2024-01-24 16:02:49 -0500

    useState para passar os parametros de busca

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,14 +1,29 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import { IoIosSearch } from 'react-icons/io';
 
 import './SearchBar.css';
+import fetchProducts from '../../api/fetchProducts';
+import AppContext from '../../context/AppContext';
 
 function SearchBar(){
-
+  
+  const {setProducts} = useContext(AppContext);
   const [searchValue ,setSearchValue] = useState('');
 
+
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+
+    const products = await fetchProducts(searchValue);
+
+    setProducts(products);
+    setSearchValue('');
+  };
+
   return (
-    <form className="search-bar">
+    <form className="search-bar" onSubmit={handleSearch}>
+      {name}
       <input type="search"
         value={searchValue}
         placeholder="Buscar produtos"
