31b53c26b24cbd86c8ef553568827a2e7dc52cf8
jeder-Bertoldo <115741406+jeder-Bertoldo@users.noreply.github.com>
2024-01-26 22:49:00 -0500

    novo componente isCartVisible

--- src/Components/CartButton/CartButton.jsx
+++ src/Components/CartButton/CartButton.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './CartButton.css';
 import { IoCartOutline } from 'react-icons/io5';
+import AppContext from '../../context/AppContext';
 
 function CartButton() {
+
+  const {cartItems, isCartVisible, setIsCartVisible} = useContext(AppContext); 
+
   return ( 
-    <button type="button" className="cart_button">
+    <button type="button" className="cart_button"
+      onClick={ () => setIsCartVisible(!isCartVisible)}
+    >
       <IoCartOutline/>
-      <span className="cart_status">1</span>
+      {cartItems.length > 0 && <span className="cart_status">{cartItems.length}</span>}
     </button>
   );
 }
+
 export default CartButton;
